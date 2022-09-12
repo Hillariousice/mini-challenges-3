cd094793a5d1d0eee56d88fd95243125fed8207f
Decagon <decagon@Decagons-MacBook-Air-2.local>
2022-09-12 13:26:57 +0100

    done

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,15 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    const valueOld =(num)=>{
+        let valueNew = Number(num).toString(2);
+        let recentValue =`${valueNew}`
+        let valuePresent = recentValue.padStart(8, '0').split("").reverse().join('')
+        let givingValue = parseInt(valuePresent,2)
+    }
+    return (givingValue.toString())
+}
 
 module.exports = binaryReversal;
+console.log(binaryReversal('47'))
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,32 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    let chainIndex = null;
+    let rankIndex = null;
+    let solutionArrIndex = null;
+    let arrowIndex = null;
+    if(Array.isArray(haystack[0])){
+        for(let i = 0; i < haystack.length; i++){
+            if(Array.isArray(haystack[i])){
+                for(let k = 0; k < haystack[i].length; k++){
+                    if(haystack[i].lastIndexOf(needle)!== -1){
+                        chainIndex = haystack.indexOf(haystack[i]);
+                        rankIndex = haystack[i].lastIndexOf(needle);
+                    }
+                }
+            }
+        }
+    }
+    else {
+        arrowIndex = haystack.lastIndexOf(needle);
+    }
+    if((chainIndex && arrowIndex !== -1) || (arrowIndex == 0)){
+        solutionArrIndex = [chainIndex, rankIndex]
+    }
+    else if((arrowIndex && arrowIndex != -1) || (arrowIndex == 0)){
+        solutionArrIndex
+    }else{
+        solutionArrIndex = -1
+    }
+    return solutionArrIndex;
+}
 
 module.exports = listSorting;
